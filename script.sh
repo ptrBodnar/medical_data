cc9f5f0c6f4942f7425953d50d9877f0911fa441
ptrBodnar <peterbodnar1213@gmail.com>
2019-09-16 00:03:39 +0300

    work in Lviv

--- script.sh
+++ script.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
+echo "doing"
 nodejs script_to_shorten_data.js money_per_month output/payments_to_doctors.json > money_per_month.json
 nodejs script_to_shorten_data.js decl_count output/payments_to_doctors.json > decl_count.json
+echo "ended"
